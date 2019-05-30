4840c8e7b6e73b8526bf0d20e18b359b87eeea42
gooder201803 <37907225+gooder201803@users.noreply.github.com>
2019-05-30 20:09:16 +0800

    Update linux_make.sh

--- linux_make.sh
+++ linux_make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 javac push_tool.java
-gcc -I/usr/lib/jvm/java-1.7.0-openjdk-amd64/include/ -fPIC -shared -o libnative.so native.c
+gcc -I/usr/lib/jvm/java-1.8.0-openjdk-amd64/include/ -I/usr/lib/jvm/java-1.8.0-openjdk-amd64/include/linux -fPIC -shared -o libnative.so native.c
 
